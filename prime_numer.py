be4af5da0f04ce945bd30fdae9f22c5487988971
sajath <zaibsajath@gmail.com>
2023-01-18 02:30:25 -0800

    03 task

---
+++ prime_numer.py
@@ -0,0 +1,20 @@
+num1=int(input("input(enter 1st number:"))
+num2=int(input("input(enter 2nd number:"))
+if num1>num2:
+    x=num1
+    y=num2
+else:
+    x=num2
+    y=num1
+
+l1=[]
+
+while (y<x+1):
+    for i in range(2,y):
+        if (y%i)==0:
+                break
+    else:
+         l1.append(y)
+    y=y+1
+print(l1)
+
