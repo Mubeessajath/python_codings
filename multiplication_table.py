0333bfd06f12e5349afc6c5dc199aeaf883b6caf
sajath <zaibsajath@gmail.com>
2023-01-21 21:32:08 -0800

    task 06

---
+++ multiplication_table.py
@@ -0,0 +1,9 @@
+num=int(input("enter the number range.....: "))
+
+for i in range(1,num+1):
+    for j in range(1,num+1):
+        total=i*j
+       
+        print(total,end=" \t ")
+   
+    print("\n")
